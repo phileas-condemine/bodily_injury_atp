5c3f54b5424c55bf8239cd323d5f21c394f70c93
phileas-condemine <phileas.condemine@axa.com>
2017-11-30 15:03:49 +0100

    check 2 grams careful underscore and 1 grams in 2 grams need to be removed

--- R/odds_ratio_ngram.R
+++ R/odds_ratio_ngram.R
@@ -3,20 +3,22 @@ library(text2vec)
 library(magrittr)
 library(dplyr)
 library(broom)
+tidy_matrix_ngram<-matrix_ngrams%>%tidy()%>%data.table()
 
-pattern="corporel"
+
+# careful, need underscore instead of space.
+key_word="tierce_personne"
+#this only handles 1 and 2 grams, not 3 grams !
+sub_key_words<-c(key_word,unlist(strsplit(key_word,"_")))
 ngram=3L
 n_cores=28
-
-tidy_matrix_ngram<-matrix_ngrams%>%tidy()%>%data.table()
-key_word="corporel"
 # key_word%in%levels(factor(tidy_matrix_ngram$column))
 tidy_matrix_ngram[,has_key_word:=1*(key_word%in%column),by=row]
 dim(tidy_matrix_ngram)
 system.time(odd_ratios<-tidy_matrix_ngram[,list("nb_docs"=.N),by=c("column","has_key_word")][,list("odd_ratio"=sum(nb_docs*(has_key_word))/sum(nb_docs*(1-has_key_word)),"volume"=sum(nb_docs)),by="column"])
 odd_ratios%>%arrange(odd_ratio)%>%
   filter(volume>100)%>%
-  mutate(has_key_word=grepl(pattern=key_word,x=.$column)>0)%>%#I don't want the ngram with the keyword inside of course !
+  mutate(has_key_word=rowSums(sapply(sub_key_words,function(x)grepl(pattern=x,x=.$column)))>0)%>%#I don't want the ngram with the keyword inside of course !
   filter(has_key_word==FALSE)%>%
   select(column,odd_ratio,volume)%>%
   tail(20)
